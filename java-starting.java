5b28b68f770b3c85c7c288c98200fb65b80d71ff
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2016-08-25 14:55:35 +0530

    Update java-starting.java

--- java-starting.java
+++ java-starting.java
@@ -6,11 +6,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        int i = 0;
-        while(i < T) {
+        int index = 0;
+        while(index < T) {
             long N = sc.nextLong();
             
-            i++;
+            index++;
         }
     }
 }
