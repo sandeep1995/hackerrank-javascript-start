69bb73d51c4e994cb942c62d9d3b7c09097fae4c
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2016-08-25 14:35:37 +0530

    Create java-starting.java

---
+++ java-starting.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int i = 0;
+        while(i < T) {
+            long N = sc.nextLong();
+            
+            i++;
+        }
+    }
+}
